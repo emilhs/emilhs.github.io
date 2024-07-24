a0e906d98b4f8ee536954ddd508da781fb39e326
Emil Hodzic-Santor <ehodzicsantor@gmail.com>
2024-07-24 10:33:04 -0600

    shiny app launch test

---
+++ build.R
@@ -0,0 +1,6 @@
+chooseapp = "/Users/emil.hodzicsantor/Documents/GitHub/emilhs.github.io/app"
+choosedest = "/Users/emil.hodzicsantor/Documents/GitHub/emilhs.github.io/docs"
+
+# Export
+
+shinylive::export(appdir = chooseapp, destdir = choosedest)
